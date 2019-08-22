73ed90150816f45d1a0ee032af87691c66b0f1be
Sławomir Golonka <sgolonka@concisesoftware.com>
2019-08-22 13:46:15 +0200

    update download script to be more configurable update download URL to use newer api (api 2)

--- src/main/groovy/com/likandr/gradle/LokalisePluginExtension.groovy
+++ src/main/groovy/com/likandr/gradle/LokalisePluginExtension.groovy
@@ -1,6 +1,21 @@
 package com.likandr.gradle
 
+import com.likandr.gradle.config.DownloadConfig
+import org.gradle.api.Project
+
 class LokalisePluginExtension {
+    private final Project project
+
     String id
     String token
+    DownloadConfig downloadConfig
+
+    LokalisePluginExtension(Project project) {
+        this.project = project
+    }
+
+    DownloadConfig downloadConfig(Closure closure) {
+        downloadConfig = new DownloadConfig()
+        project.configure(downloadConfig, closure)
+    }
 }
